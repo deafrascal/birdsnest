a91ac3fe3100f8d7c189e27d0917e9b08d22f543
Joshua Peek <josh@joshpeek.com>
2014-10-12 17:20:39 -0700

    Include all packages

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 mkdir -p packages/
 
-bower search test | tail -n+2 | while read line
+bower search | tail -n+2 | while read line
 do
   name=$(echo $line | cut -d ' ' -f1)
   url=$(echo $line | cut -d ' ' -f2)
