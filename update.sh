39e7b715b6717f33aaa781bac53e70880aa0bfc3
Joshua Peek <josh@joshpeek.com>
2014-10-12 17:18:46 -0700

    Add update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p packages/
+
+bower search test | tail -n+2 | while read line
+do
+  name=$(echo $line | cut -d ' ' -f1)
+  url=$(echo $line | cut -d ' ' -f2)
+
+  if [ -n "$name" ]; then
+    echo $url > "packages/$name"
+  fi
+done
